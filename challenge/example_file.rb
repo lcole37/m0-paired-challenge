72678fca768b70768f5bf5ed8e3c1e9795c5d97d
Lucas Colwell <colwellc37@gmail.com>
2022-02-09 20:23:56 -0700

    Add books

---
+++ challenge/example_file.rb
@@ -0,0 +1,10 @@
+# quiclky came up with name for our example file. we worked separately not driver/nav
+
+pages = 425
+publisher = "Printing house"
+is_fiction = true
+lines_on_pg_1 = 22.5
+title_1 = "Harry Potter"
+hardcover = true
+
+genres = ["Scifi", "comedy", "thriller"]
